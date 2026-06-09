b073a2625b1320f4afd6b370d6d50c5bb5acf106
ProgettoHydraTech <fede.sebastiani0203@gmail.com>
2026-06-09 13:24:41 +0200

    Nascondi navbar quando embeddato in WordPress

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,12 @@ import OperatorTabs from './components/OperatorTabs'
 import PhotoGallery from './components/PhotoGallery'
 import Footer from './components/Footer'
 
+const isEmbedded = !!window.__HYDRAFIBER_MOUNT__
+
 export default function App() {
   return (
     <>
-      <Navbar />
+      {!isEmbedded && <Navbar />
       <Hero />
       <div className="divider" />
       <ComparisonSection />
